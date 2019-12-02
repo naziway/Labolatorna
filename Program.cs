c52428a76cf79c8c905e525e4d8c4bc147271d64
naziway <nazarAAAA@ukr.net>
2019-12-02 12:48:46 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Labolatorna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! From Nazar");
+
+            Console.ReadLine();
+        }
+    }
+}
